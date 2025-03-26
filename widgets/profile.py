5924544146f8069f4b4012d2b0abc7cf08f7f782
kai <kai.park@emory.edu>
2025-03-26 02:56:26 -0400

    less janky pfp

--- widgets/profile.py
+++ widgets/profile.py
@@ -1,31 +1,44 @@
-from fabric.widgets.box import Box
-from fabric.widgets.label import Label
-from fabric.widgets.datetime import DateTime
 
-from fabric import Fabricator
+import gi
+gi.require_version("Gtk", "3.0")
+gi.require_version("GdkPixbuf", "2.0")
+from gi.repository import Gtk, GdkPixbuf, GLib
 
 import os
+import time
+import psutil
 from loguru import logger
-
-import time, psutil
+from fabric import Fabricator
 
 from user.icons import Icons
 
-
-def get_profile_picture_path() -> str | None:
+def get_profile_picture_pixbuf(size=96):
     path = os.path.expanduser("~/Pictures/profile.jpg")
     if not os.path.exists(path):
         path = os.path.expanduser("~/.face")
     if not os.path.exists(path):
         logger.warning("put yo fuckin picture in ~/Pictures/profile.jpg or ~/.face")
-        path = None
-    return path
+        return None
 
+    pixbuf = GdkPixbuf.Pixbuf.new_from_file(path)
 
-class Profile(Box):
+    width = pixbuf.get_width()
+    height = pixbuf.get_height()
+    size_to_crop = min(width, height)
+
+    x_offset = (width - size_to_crop) // 2
+    y_offset = (height - size_to_crop) // 2
+
+    cropped_pixbuf = pixbuf.new_subpixbuf(x_offset, y_offset, size_to_crop, size_to_crop)
+
+    resized_pixbuf = cropped_pixbuf.scale_simple(size, size, GdkPixbuf.InterpType.BILINEAR)
+
+    return resized_pixbuf
+
+class Profile(Gtk.Box):
     @staticmethod
     def psutil_uptime(f: Fabricator):
-        while 1:
+        while True:
             elapsed = int(time.time() - psutil.boot_time())
             days, remainder = divmod(elapsed, 86400)
             hours, remainder = divmod(remainder, 3600)
@@ -35,43 +48,49 @@ class Profile(Box):
 
     cool_fabricator = Fabricator(poll_from=psutil_uptime, stream=True)
 
-    def __init__(self, **kwargs) -> None:
-        super().__init__(orientation="h", h_expand=True, **kwargs)
+    def __init__(self, **kwargs):
+        super().__init__(orientation=Gtk.Orientation.HORIZONTAL, expand=True, **kwargs)
+        
+        self.profile_pic = Gtk.Image(name="profile-pic")
+        pixbuf = get_profile_picture_pixbuf()
+        if pixbuf:
+            self.profile_pic.set_from_pixbuf(pixbuf)
+        else:
+            self.profile_pic.set_from_icon_name("avatar-default", Gtk.IconSize.DIALOG)
 
-        self.profile_pic = Box(
-            name="profile-pic",
-            style=f'background-image: url("file://{get_profile_picture_path() or ""}")',
-        )
-        self._labels_container = Box(
-            orientation="v",
-            h_align="fill",
-            v_align="center",
-            h_expand=True,
-            v_expand=True,
-        )
-        self.username = Label(
-            label=os.getlogin().title(),
-            style_classes="username",
-            h_align="start",
-        )
 
-        self.date = DateTime(
-            formatters=(Icons.CALENDAR.value + " %A %m/%d/%Y"), style_classes="date", 
-            h_align="start",
-        )
+        self.username = Gtk.Label(label=os.getlogin().title())
+        self.username.set_xalign(0)  
+        self.username.get_style_context().add_class("username")
 
-        self.uptime = Label(label="", style_classes="uptime", 
-            h_align="start",)
-        self.cool_fabricator.connect(
-            "changed",
-            self.update_status
-        )
+        self.date = Gtk.Label(label=time.strftime(Icons.CALENDAR.value + " %A %m/%d/%Y"))
+        self.date.set_xalign(0)
+        self.date.get_style_context().add_class("date")
+        GLib.timeout_add(1000, self.update_date_label)
 
-        self.add(self.profile_pic)
-        self._labels_container.add(self.username)
-        self._labels_container.add(self.date)
-        self._labels_container.add(self.uptime)
-        self.add(self._labels_container)
+        self.uptime = Gtk.Label(label="")
+        self.uptime.set_xalign(0)
+        self.uptime.get_style_context().add_class("uptime")
 
-    def update_status(self, f: Fabricator, value: float):
-        self.uptime.set_label(Icons.TIMER.value + " " + str(value))
+        self.cool_fabricator.connect("changed", self.update_status)
+
+        self._labels_container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, expand=True)
+        self._labels_container.set_valign(Gtk.Align.CENTER)
+
+        self._labels_container.pack_start(self.username, False, False, 0)
+        self._labels_container.pack_start(self.date, False, False, 0)
+        self._labels_container.pack_start(self.uptime, False, False, 0)
+
+        self.pack_start(self.profile_pic, False, False, 0)
+        self.pack_start(self._labels_container, True, True, 6)
+
+    def update_status(self, f: Fabricator, value: str):
+        """Update uptime label dynamically."""
+        self.uptime.set_text(Icons.TIMER.value + " " + value)
+
+    def update_date_label(self):
+        """Update the date label every second."""
+        current_date = time.strftime(Icons.CALENDAR.value + " %A %m/%d/%Y")
+        self.date.set_text(current_date)
+        return True  
+
